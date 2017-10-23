fc3609bd497402853cd9344ea9c42cff50720991
aisayo <aisayo@icloud.com>
2017-10-23 21:25:55 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,19 +16,22 @@ class PostsController < ApplicationController
   @post.save
   redirect_to post_path(@post)
 end
- 
+
 def update
   @post = Post.find(params[:id])
   @post.update(post_params)
   redirect_to post_path(@post)
 end
- 
+
+def edit
+	@post = Post.find(params[:id])
+end
+
 private
- 
+
 def post_params
   params.require(:post).permit(:title, :description)
 end
-	def edit
-	  @post = Post.find(params[:id])
-	end
+
+	
 end
